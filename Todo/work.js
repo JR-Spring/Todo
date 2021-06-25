2ea197c9cd859a3acaf791611f5395b5ceff4d13
JR Springe <jr.spring55@gmail.com>
2021-06-24 23:37:46 -0500

    Created elements and appended to div

--- Todo/work.js
+++ Todo/work.js
@@ -1,31 +1,36 @@
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todoList');
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-List");
 
 todoButton.addEventListener("click", addTodo);
 
-Event.preventDefault();
+//Created elements in JS versus HTML, appended created elements to div 
+
+function addTodo(event){
 
 
-const addTodo = event => {
+event.preventDefault();
 
 const todoDiv = document.createElement("div");
 todoDiv.classList.add("todo");
 
 const newTodo = document.createElement("li");
-newTodo.innerText = 'hello';
+newTodo.innerText = "what it do";
 newTodo.classList.add('todo-item');
 todoDiv.appendChild(newTodo);
-}
 
-// Check mark button
-const completedButon = document.createElement("button");
-completedButon.innerHTML = '<i class= "fas fa-check"></i>';
-completedButon.classList.add("complete-btn");
-todoDiv.appendChild(completedButon);
 
-//Check trash button
+// Check mark button when items completed
+const completedButton = document.createElement("button");
+completedButton.innerHTML = '<i class= "fas fa-check"></i>';
+completedButton.classList.add("complete-btn");
+todoDiv.appendChild(completedButton);
+
+// Trash button
 const trashButton = document.createElement('button');
 trashButton.innerHTML = '<i class= "fas fa-trash"></i>';
 trashButton.classList.add("complete-btn");
 todoDiv.appendChild(trashButton);
+
+todoList.appendChild(todoDiv);
+}
