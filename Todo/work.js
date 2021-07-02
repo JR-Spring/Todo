dafb0d6c1ae1604d563fc14c08da79dadd86e273
JR Springe <jr.spring55@gmail.com>
2021-07-02 00:57:56 -0500

    Added functional delete button

--- Todo/work.js
+++ Todo/work.js
@@ -1,18 +1,26 @@
+// Selectors
+
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-List");
+document.createElement('script').src = 'http://Todo/work.js';
 
+// Event Listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
-//Created elements in JS versus HTML, appended created elements to div 
-
+//Created function and created elements in JS versus HTML, appended created elements to div 
 function addTodo(event){
 
-
+// Prevent form from submitting
 event.preventDefault();
+
+// Todo Div
 
 const todoDiv = document.createElement("div");
 todoDiv.classList.add("todo");
+
+//
 
 const newTodo = document.createElement("li");
 newTodo.innerText = todoInput.value;
@@ -29,8 +37,20 @@ todoDiv.appendChild(completedButton);
 // Trash button
 const trashButton = document.createElement('button');
 trashButton.innerHTML = '<i class= "fas fa-trash"></i>';
-trashButton.classList.add("complete-btn");
+trashButton.classList.add("trash-btn");
 todoDiv.appendChild(trashButton);
 
 todoList.appendChild(todoDiv);
+
+//Clears input field after adding todo item to list 
+todoInput.value = "";
+}
+
+function deleteCheck(e){ 
+const item = e.target;
+if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+    }
+
 }
