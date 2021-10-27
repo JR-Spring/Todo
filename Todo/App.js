0eee9a949835a8a6764a7312bdedd5ed557e2afd
Jr-Spring <jr.spring55@gmail.com>
2021-10-26 22:07:30 -0500

    Clean up

--- Todo/App.js
+++ Todo/App.js
@@ -15,7 +15,6 @@ todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener('click', filterTodo);
 document.addEventListener('DOMContentLoaded', getTodos )
 
-
 //***************Functions******************
 function addTodo(event){
 // Prevents form from submitting
@@ -60,17 +59,15 @@ if (item.classList[0] === "trash-btn") {
     removeLocalTodos(todo);
     todo.remove();
 }
-
 // Check mark
 if (item.classList[0] === "complete-btn") {
 const todo = item.parentElement;
 todo.classList.toggle("completed")
-}
-
+    }
 };
 
 
-// Filter options All, Complete, Incomplete in drop down 
+// Filter options All, Complete, Incomplete in drop down menu
  function filterTodo(e){
     const todos = todoList.childNodes;
     todos.forEach(function(todo) {
@@ -162,4 +159,42 @@ function removeLocalTodos(todo) {
     todos.splice(todos.indexOf(todoIndex), 1);
     localStorage.setItem('todos',JSON.stringify(todos));
    
+}
+
+function getTodos(){
+    
+   let todos;
+
+   if (localStorage.getItem('todos') === null) {
+       todos = [];
+   }
+   else {
+       todos = JSON.parse(localStorage.getItem('todos'));
+    }
+   todos.forEach(function(todo){
+       //Todo Div
+       const todoDiv = document.createElement("div");
+       todoDiv.classList.add("todo");
+       
+       //Created Li
+       const newTodo = document.createElement("li");
+       newTodo.innerText =todo; 
+       newTodo.classList.add('todo-item');
+       todoDiv.appendChild(newTodo);
+       
+       // Check mark button when items completed
+       const completedButton = document.createElement("button");
+       completedButton.innerHTML = '<i class= "fas fa-check"></i>';
+       completedButton.classList.add("complete-btn");
+       todoDiv.appendChild(completedButton);
+       
+       // Trash button
+       const trashButton = document.createElement('button');
+       trashButton.innerHTML = '<i class= "fas fa-trash"></i>';
+       trashButton.classList.add("trash-btn");
+       todoDiv.appendChild(trashButton);
+       
+       // Add to do to list
+       todoList.appendChild(todoDiv);
+   });
 }
